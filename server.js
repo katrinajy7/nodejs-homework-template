fffeec45d58b7ba8a22204e6434ffc913715534f
katrinajy7 <145580105+katrinajy7@users.noreply.github.com>
2024-10-13 13:07:07 +0800

    update

--- server.js
+++ server.js
@@ -1,5 +1,34 @@
-const app = require("./app");
+// const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
+// app.listen(3000, () => {
+//   console.log("Server is running. Use our API on port: 3000");
+// });
+
+// const app = require("./.git");
+import { app } from "./app.js"
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+
+//initialize the environment variables
+dotenv.config();
+// access the environment variables
+
+const { DB_HOST} = process.env;
+
+mongoose.connect(DB_HOST, {
+  // useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useUnifiedTopology: true,
+})
+.then (() => {
+  app.listen(3000, () => {
+    console.log("Server is running. Use our API on port: 3000");
 });
+
+console.log("Database connect successful");
+})
+.catch((error) => {
+  console.log(`Server not running. Error message: ${error.message}`);
+  process.exit(1) // this terminates the mongoose connection
+})
